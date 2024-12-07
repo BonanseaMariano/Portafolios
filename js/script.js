2109683cffe2160b075e349ee83e8f071dab434d
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2024-12-06 22:39:23 -0300

    Primera tanda de animaciones

--- js/script.js
+++ js/script.js
@@ -44,5 +44,16 @@ ScrollReveal({
     delay: 200
 });
 
-ScrollReveal.reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Desarrollador Fullstack', 'Desarrollador Android'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
