c3ffd002a6143ef6eb5ebb3272c45264371b9f22
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2024-12-09 18:31:09 -0300

    Contenido p e imagenes

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Desarrollador Fullstack', 'Desarrollador Android'],
+    strings: ['Aspirante a Desarrollador Fullstack', 'Aspirante a Desarrollador de Apps'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
